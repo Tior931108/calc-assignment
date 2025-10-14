10892534da5086c6337850be9bd01fc0d9bc491d
Tior931108 <jyj8469@gmail.com>
2025-10-14 23:05:21 +0900

    필수:lv.1 계산기 과제 프로젝트 setting

---
+++ src/lv1calculator/Calculator.java
@@ -0,0 +1,17 @@
+package lv1calculator;
+/*
+
+Lv 1.클래스 없이 자바의 기본 문법만을 사용하여 구현한 계산기
+
+[요약]
+a. 계산기는 2개의 숫자를 받을 수 있고 사칙연산 될 문자를 받을 수 있다.
+b. 계산기는 exit을 입력할 때까지 계속해서 값을 받고 연산 결과를 변환한다.
+
+ */
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Hello, Calculator!");
+        System.out.println("=================Lv1 클래스 없이 구현하는 게산기 과제=================");
+    }
+}
