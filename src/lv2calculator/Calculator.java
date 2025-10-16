89c087b907f853537628484ab462b442cb1c22fb
Tior931108 <jyj8469@gmail.com>
2025-10-16 14:17:42 +0900

    필수:lv.2 3)App 클래스의 main 메소드에서 Calculator클래스의 연산 결과를 저장하고 있는 컬렉션 필드에 직접 접근하지 못하도록 수정 및 가장 먼저 삭제하는 메서드 구현

--- src/lv2calculator/Calculator.java
+++ src/lv2calculator/Calculator.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-    // 연산 결과를 저장하는 컬렉션 타입 필드
-    public List<Integer> results = new ArrayList<>();
+    // 연산 결과를 저장하는 컬렉션 타입 필드 - private 사용하여 캡슐화
+    private List<Integer> results = new ArrayList<>();
 
     /**
      * 사칙 연산 수행 메서드
-     * @param첫번째 양의 정수(0포함) 숫자
-     * @param두번째 양의 정수(0포함) 숫자
-     * @param연산자
+     * @param num1 첫번째 양의 정수(0포함) 숫자
+     * @param num2 두번째 양의 정수(0포함) 숫자
+     * @param operator 연산자
      * @return 계산된 값
      */
     public int calculate(int num1, int num2, char operator) {
@@ -48,5 +48,26 @@ public class Calculator {
     }
 
 
-    // 지금까지의 연산 결과 리스트를 반환하는 메서드
+    // 간접 접근을 통해 필드에 접근하여 가져오는 메소드 - getter
+    public List<Integer> getResults() {
+        return results;
+    }
+
+
+    // 간접 접근을 통해 필드에 접근하여 수정하는 메소드 - setter
+    public void setResults(List<Integer> results) {
+        this.results = results;
+    }
+
+    // 가장 먼저 저장된 데이터를 삭제하는 메소드
+    public void removeResult() {
+        if (!results.isEmpty()) {
+            // 저장된 데이터가 있다면 첫번째 요소 삭제 : 인덱스 0
+            results.remove(0);
+            System.out.println("가장 먼저 저장된 값이 삭제되었습니다.");
+        } else {
+            // 저장된 데이터가 없다면
+            System.out.println("삭제할 결과값이 없습니다.");
+        }
+    }
 }
