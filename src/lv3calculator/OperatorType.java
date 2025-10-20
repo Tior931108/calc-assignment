fcb06627465bd87e97924ba18596636ea63b0f4a
Tior931108 <jyj8469@gmail.com>
2025-10-20 17:36:59 +0900

    ♻ refactor(Operator): [도전 Lv.3] Enum 상수 람다식으로 변경
    - 기존 추상클래스 상속 받아 구현한 익명 클래스 변경
    - 코드 가독성 증대하기 위해 람다식으로 변환

--- src/lv3calculator/OperatorType.java
+++ src/lv3calculator/OperatorType.java
@@ -1,69 +1,62 @@
 package lv3calculator;
 
+import java.util.function.BiFunction;
+
 public enum OperatorType {
 
     /**
-     * 각각 사칙연산을
-     * 추상 메소드를 상속 받아서 구현.
-     * 각 Enum 상수가 익명 클래스처럼 동작한다.
-     * 제네릭을 활용하여 타입 안정성을 확보하면서도 유연한 계산 가능
+     * 람다식을 활용한 사칙 연산 구현
+     * 각 Enum 상수가 BiFunction 함수형 인터페이스를 통해 연산 로직을 간경하게 표현
+     *
+     * @funtionalInterface가 이미 선언된 표준 함수형 인터페이스
+     * BiFunction <Number, Number, Number> : 2개의 Number를 받아서 Number를 반환하는 함수형 인터페이스
      */
-    PLUS('+') {
-        @Override
-        public <T extends Number> T apply(Number num1, Number num2) {
-            // 둘 중 하나라도 실수(Double) 이면 Double연산
-            if (num1 instanceof Double || num2 instanceof Double) {
-                // 1. num1.doubleValue() + num2.doubleValue() : double형 기본형
-                // 2. Double.valueOf(...) : Double Wrapper 클래스로 감싸주기
-                // 3. (T)는 Number 하위 타입(Double)이므로 안전하게 캐스팅이 가능
-                return (T) Double.valueOf(num1.doubleValue() + num2.doubleValue());
-            }
-            // 둘 다 정수(Integer)라면 Integer 연산
-            return (T) Integer.valueOf(num1.intValue() + num2.intValue());
+    PLUS('+', (num1, num2) -> {
+        // 둘 중 하나라도 실수(Double) 이면 Double연산
+        if (num1 instanceof Double || num2 instanceof Double) {
+            // 1. num1.doubleValue() + num2.doubleValue() : double형 기본형
+            // 2. Double.valueOf(...) : Double Wrapper 클래스로 감싸주기
+            return Double.valueOf(num1.doubleValue() + num2.doubleValue());
         }
-    },
+        // 둘 다 정수(Integer)라면 Integer 연산
+        return Integer.valueOf(num1.intValue() + num2.intValue());
+    }),
 
-    MINUS('-') {
-        @Override
-        public <T extends Number> T apply(Number num1, Number num2) {
-            if (num1 instanceof Double || num2 instanceof Double) {
-                return (T) Double.valueOf(num1.doubleValue() - num2.doubleValue());
-            }
-            return (T) Integer.valueOf(num1.intValue() - num2.intValue());
+    MINUS('-', (num1, num2) -> {
+        if (num1 instanceof Double || num2 instanceof Double) {
+            return Double.valueOf(num1.doubleValue() - num2.doubleValue());
         }
-    },
+        return Integer.valueOf(num1.intValue() - num2.intValue());
+    }),
 
-    MULTIPLY('*') {
-        @Override
-        public <T extends Number> T apply(Number num1, Number num2) {
-            if (num1 instanceof Double || num2 instanceof Double) {
-                return (T) Double.valueOf(num1.doubleValue() * num2.doubleValue());
-            }
-            return (T) Integer.valueOf(num1.intValue() * num2.intValue());
+    MULTIPLY('*', (num1, num2) -> {
+        if (num1 instanceof Double || num2 instanceof Double) {
+            return Double.valueOf(num1.doubleValue() * num2.doubleValue());
         }
-    },
+        return Integer.valueOf(num1.intValue() * num2.intValue());
+    }),
 
-    DIVIDE('/') {
-        @Override
-        public <T extends Number> T apply(Number num1, Number num2) {
-            if (num2.doubleValue() == 0.0) {
-                // 0으로 나눌수 없는 조건 main에서 예외처리
-                throw new ArithmeticException("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
-            }
-            // 나눗셈은 정확한 결과를 위해서 항상 Double로 반환
-            return (T) Double.valueOf(num1.doubleValue() / num2.doubleValue());
+    DIVIDE('/', (num1, num2) -> {
+        if (num2.doubleValue() == 0.0) {
+            // 0으로 나눌수 없는 조건 main에서 예외처리
+            throw new ArithmeticException("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
         }
-    };
+        // 나눗셈은 정확한 결과를 위해서 항상 Double로 반환
+            return Double.valueOf(num1.doubleValue() / num2.doubleValue());
+    });
 
     private final char symbol;
+    private final BiFunction<Number, Number, Number> operation;
 
     /**
-     * Operater 생성자
+     * Operator 생성자
      *
      * @param symbol 연산자 기호
+     * @param operation 람다식으로 표현된 연산 함수 (BiFunction 함수형 인터페이스)
      */
-    OperatorType(char symbol) {
+    OperatorType(char symbol, BiFunction<Number, Number, Number> operation) {
         this.symbol = symbol;
+        this.operation = operation;
     }
 
     // 연산자 기호 반환
@@ -90,15 +83,13 @@ public enum OperatorType {
 
     /**
      * 실제 연산을 수행하는 추상 메소드
-     * 사칙연산이 [숫자] [연산자] [숫자] 방식처럼
-     * 이항 연산의 틀을 사용하고 있어서
-     * 공통된 계산의 틀을 강제하는 방식의 추상메소드 사용
+     * 람다식으로 정의된 operation 함수형 인터페이스를 실행
      *
      * 제네릭 메소드로 구현하여 타입 안정성 확보
-     * [ 정수 ] [ 연산자 ] [ 정수 ]
-     * [ 실수 ] [ 연산자 ] [ 정수 ]
-     * [ 정수 ] [ 연산자 ] [ 실수 ]
-     * [ 실수 ] [ 연산자 ] [ 실수 ]
+     * [ 정수 ] [ 연산자 ] [ 정수 ] > Integer
+     * [ 실수 ] [ 연산자 ] [ 정수 ] > Double
+     * [ 정수 ] [ 연산자 ] [ 실수 ] > Double
+     * [ 실수 ] [ 연산자 ] [ 실수 ] > Double
      *
      * 나눗셈은 항상 Double 반환
      *
@@ -107,6 +98,10 @@ public enum OperatorType {
      * @param <T>  반환 타입(Number를 상속 받는 타입)
      * @return 연산 결과
      */
-    public abstract <T extends Number> T apply(Number num1, Number num2);
+    public <T extends Number> T apply(Number num1, Number num2) {
+        // Bifuction의 apply 메소드를 호출하려 람다식 실행
+        // (T)는 Number 하위 타입(Double 이거나 Integer)이므로 안전하게 캐스팅이 가능
+        return (T) operation.apply(num1, num2);
+    }
 
 }
